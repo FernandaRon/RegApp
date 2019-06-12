6f783058b81023a2750c0c37aa9144bd521487dd
FernandaRon <rondon.fernanda@gmail.com>
2019-06-11 23:34:37 -0500

    import firebase,fbdatabase and environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+//firebase 
+import {AngularFireModule } from 'angularfire2';
+import { AngularFireDataBaseModule } from 'angulargire2/database'; 
+import { environment } from '../enviroments/enviroment';
+
+//components
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(enviroment.firebase),
+    AngularFireDataBaseModule,
+    Forms
   ],
   providers: [],
   bootstrap: [AppComponent]
